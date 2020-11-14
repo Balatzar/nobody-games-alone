dd4ba9c57f30fb38935635897416994349ff4737
balatzar <bataprod@gmail.com>
2020-11-14 14:00:17 +0100

    Postgres dont rejext ssl

--- db/index.js
+++ db/index.js
@@ -1,5 +1,9 @@
 const { Pool } = require("pg");
-const pool = new Pool({ ssl: true });
+const pool = new Pool({
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 module.exports = {
   query: (text, params) => {
     console.log(text);
