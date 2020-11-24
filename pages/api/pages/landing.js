1fb11c722c7ef346db2fdcc693e5768eedbf4ae3
balatzar <bataprod@gmail.com>
2020-11-24 17:33:01 +0100

    Limit landing

--- pages/api/pages/landing.js
+++ pages/api/pages/landing.js
@@ -6,13 +6,14 @@ const handler = async (req, res) => {
   const fetchPlatforms = await db.query(`
     SELECT platforms.*, games.slug as g_slug, games.id as g_id, games.name as g_name FROM platforms
     INNER JOIN games_users_platforms ON games_users_platforms.platform_id = platforms.id
-    INNER JOIN games ON games.id = games_users_platforms.game_id;
+    INNER JOIN games ON games.id = games_users_platforms.game_id
+    LIMIT 4;
   `);
 
   const fetchGames = await db.query(`
     SELECT games.* from games
     WHERE cover_image_id != ''
-    LIMIT 10;
+    LIMIT 4;
   `);
 
   res.status(200).json({
